48a311cf6da99d3ae51a6debdd897a41a17b30f4
Julia García Martínez <jgm139@alu.ua.es>
2018-12-12 22:18:14 +0100

    Aumentando dataset

--- BBoT/src/take_images/src/camera_subscriber.py
+++ BBoT/src/take_images/src/camera_subscriber.py
@@ -16,29 +16,34 @@ class TakeImageNode:
         self.sub = rospy.Subscriber('/xtion/rgb/image_raw', Image, self.take_images_callback)
         print("Setting up publisher: /mobile_base_controller/cmd_vel")
         self.move = rospy.Publisher('/mobile_base_controller/cmd_vel', Twist, queue_size=1)
+        self.rate = rospy.Rate(5)
         self.lastImageTaken = None
-        self.numImage = 0
+        self.numImage = 86
         self.width = 608
         self.height = 608
         self.photo = False
+        self.loop = False
 
     def take_images_callback(self, data):
    
         try:
             if self.photo:
                 print("Saving the last photography")
-                self.photo = False
+
+                if not self.loop:
+                    self.photo = False
                 cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
                 # cv2.imshow("Image in rgb",cv_image)
                 cv2.waitKey(3)
                 self.lastImageTaken = cv2.resize(cv_image, (self.width, self.height))
                 # image_gray = cv2.cvtColor(self.lastImageTaken, cv2.COLOR_BGR2GRAY)
             
-                fileName = "/home/juliagm/Documentos/B-BoT/dataset/Images/009/Image" + str(self.numImage) + ".jpg"
+                fileName = "/home/juliagm/Documentos/B-BoT/dataset/Images/002/Image" + str(self.numImage) + ".jpg"
 
                 cv2.imwrite(fileName, self.lastImageTaken)
 
                 self.numImage+=1
+                self.rate.sleep()
         except CvBridgeError as e:
             print(e)
 
@@ -54,20 +59,23 @@ class TakeImageNode:
                     if event.type == QUIT: sys.exit()
 
                     if event.type == KEYDOWN and event.key == 119: # W - straight
-                        move_cmd.linear.x += 0.5
+                        move_cmd.linear.x += 0.65
                     elif event.type == KEYDOWN and event.key == 97: # A - left
-                        move_cmd.angular.z += 0.25
+                        move_cmd.angular.z += 0.35
                         move_cmd.linear.x = 0.0
                     elif event.type == KEYDOWN and event.key == 100: # D - right
-                        move_cmd.angular.z += -0.25
-                    elif event.type == KEYDOWN and event.key == 115: # S - back
-                        move_cmd.linear.x += -0.5
-                        move_cmd.linear.x = 0.0
+                        move_cmd.angular.z += -0.35
                     elif event.type == KEYDOWN and event.key == 112: # P - take photo
                         self.photo = not self.photo
                     elif event.type == KEYDOWN and event.key == 122: # Z - stop
                         move_cmd.linear.x = 0.0
                         move_cmd.angular.z = 0.0
+                    elif event.type == KEYDOWN and event.key == 108: # L - loop
+                        self.loop = True    
+                        self.photo = True
+                    elif event.type == KEYDOWN and event.key == 115: # S - stop loop
+                        self.loop = False    
+                        self.photo = False
 
                 pygame.event.pump()
                 self.move.publish(move_cmd)
@@ -79,8 +87,8 @@ class TakeImageNode:
 def main(args):
     pygame.init()
     pygame.display.set_mode((200,200))
-    tI = TakeImageNode()
     rospy.init_node('take_images_node')
+    tI = TakeImageNode()
     tI.run()
     cv2.destroyAllWindows()
     rospy.spin()
