058879887ff208e12d0e32630719610742069c88
Julia García Martínez <jgm139@alu.ua.es>
2018-12-15 17:47:06 +0100

    Dataset de 1240 imágenes

--- BBoT/src/take_images/src/camera_subscriber.py
+++ BBoT/src/take_images/src/camera_subscriber.py
@@ -18,7 +18,7 @@ class TakeImageNode:
         self.move = rospy.Publisher('/mobile_base_controller/cmd_vel', Twist, queue_size=1)
         self.rate = rospy.Rate(5)
         self.lastImageTaken = None
-        self.numImage = 74
+        self.numImage = 79
         self.width = 608
         self.height = 608
         self.photo = False
@@ -37,7 +37,7 @@ class TakeImageNode:
                 self.lastImageTaken = cv2.resize(cv_image, (self.width, self.height))
                 # image_gray = cv2.cvtColor(self.lastImageTaken, cv2.COLOR_BGR2GRAY)
             
-                fileName = "/home/juliagm/Documentos/B-BoT/dataset/Images/008/" + str(self.numImage) + ".jpg"
+                fileName = "/home/juliagm/Documentos/B-BoT/dataset/Images/010/" + str(self.numImage) + ".jpg"
                 print("Saving the last photography " + fileName)
                 cv2.imwrite(fileName, self.lastImageTaken)
 
