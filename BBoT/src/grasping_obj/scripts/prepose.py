e40644ed874954fc7cac1a2d3f1c11bf5532d528
Julia García Martínez <jgm139@alu.ua.es>
2019-04-22 20:02:01 +0200

    Prepose

--- BBoT/src/grasping_obj/scripts/prepose.py
+++ BBoT/src/grasping_obj/scripts/prepose.py
@@ -10,19 +10,18 @@ from geometry_msgs.msg import Point
 from tf import TransformListener
 
 class Prepose:
-    ARM_TORSO_JOINTS = [1.5, 0.25, 0, -2, 1.5, 0, 0, 0]
+    ARM_TORSO_JOINTS = [0.35, 0.25, 0, -2, 1, 0, 0, 0]
 
     def __init__(self):
-        self.rate = rospy.Rate(1.0)
-        self.rate.sleep()
-
         self.arm_torso_group = moveit_commander.MoveGroupCommander("arm_torso")
         self.arm_torso_joints_values = self.arm_torso_group.get_current_joint_values()
 
     def prepose(self):
 
         for i in range(0, len(self.ARM_TORSO_JOINTS)):
+            print i, ", ", self.ARM_TORSO_JOINTS[i]
             self.arm_torso_joints_values[i] = self.ARM_TORSO_JOINTS[i]
+            print self.arm_torso_joints_values[i]
 
         self.arm_torso_group.go(joints=self.arm_torso_joints_values)
 
